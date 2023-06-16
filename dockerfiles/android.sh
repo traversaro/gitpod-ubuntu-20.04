9adb55fd00cfb08b2921225fa701ec1dc994f54b
Syahroni Wahyu <136780322+edufunesia@users.noreply.github.com>
2023-06-16 20:25:11 +0700

    Create android.sh

---
+++ dockerfiles/android.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+sudo apt-get update -y
+sudo apt-get install android-sdk lib32stdc++6 android-studio android-sdk-build-tools android-sdk android-sdk-platform-23 --no-install-recommends --yes
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
